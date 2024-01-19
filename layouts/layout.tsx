44d215eee546175f7c660858b5fbd08c0af6f3ba
UzairMalik17 <uzairam17@gmail.com>
2024-01-19 20:58:00 +0500

    dashboard layout

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -2,17 +2,28 @@ import React from "react";
 import Navbar from "@/components/Client/Navigation/ClientNavigation";
 import Footer from "@/components/Client/Footer/Footer";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 type Props = {
   children: React.ReactNode;
 };
+
 const layout: React.FC<Props> = ({ children }: Props) => {
+  const Router = useRouter();
+  const AdminRoute = Router.pathname.includes("/admin");
   return (
-    <div className={`w-full h-full ${inter.className}`}>
-      <Navbar />
-      <div className={`w-full h-full `}>
+    <div
+      className={`w-full h-full grid ${
+        AdminRoute && "md:grid-cols-[250px,calc(100%-258px)]"
+      } ${inter.className}`}>
+      {!AdminRoute && <Navbar />}
+      {AdminRoute && (
+        <div className={`w-full h-screen bg-slate-600`}>sidebar</div>
+      )}
+      <div className={`w-full h-full`}>
         <main>{children}</main>
+        main screen
         <Footer />
       </div>
     </div>
